61eed1286cbf24ed9827895988c88e09a45bdb72
Michael F. Canzoneri <mikecanzoneri@gmail.com>
2011-10-25 03:35:13 +0000

    Add php-pecl-ssh2 and some documentation

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,16 +1,24 @@
 include baseline
 
-node default {
+# webserver                     Basic webserver that might use some other data store or may just not need one
+#
+# devserver|mysql-webserver     Servers that include mysql.  We have two types because webserver will just 
+#                               have ports 80 and 443 open while the devserver will also have port 22.
+#
+# admin                         Admin and build machine.  If one wants to ssh into a webserver they need
+#                               to go through this machine.  Also hosts the private yum repo
+
+node default { 
     if $ec2_security_groups =~ /webserver/ {
         include httpd
         include yum
         include php
-        include libssh2
+        include php-pecl-ssh2
     } elsif $ec2_security_groups =~ /(devserver|mysql-webserver)/ {
         include httpd
         include yum
         include php
-        include libssh2
+        include php-pecl-ssh2
         include mysql
     } elsif $ec2_security_groups =~ /admin/ {
         include build
@@ -23,4 +31,3 @@ node default {
         notify {"No definition for security group: $ec2_security_groups":}
     }
 }
-
---
+++ modules/php-pecl-ssh2/manifests/init.pp
@@ -0,0 +1,6 @@
+class php-pecl-ssh2 {
+
+    package { ["php-pecl-ssh2"]:
+        ensure => installed,
+    }
+}
