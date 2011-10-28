c427f571a7d96cb0cc8e76a98774fcf7cd7ff6bf
Michael F. Canzoneri <mikecanzoneri@gmail.com>
2011-10-28 19:33:15 +0000

    Update readme and remove internal.repo for now

--- modules/yum/manifests/init.pp
+++ modules/yum/manifests/init.pp
@@ -7,12 +7,12 @@ class yum {
         ensure => latest,
     }
   
-    file { "/etc/yum.repos.d/internal.repo":
-        owner => "root",
-        group => "root",
-        mode  => 644,
-        content => template("yum/internal.repo.erb"),
-    }
+    #file { "/etc/yum.repos.d/internal.repo":
+    #    owner => "root",
+    #    group => "root",
+    #    mode  => 644,
+    #    content => template("yum/internal.repo.erb"),
+    #}
 
     file { "/etc/yum.repos.d/external.repo":
         source => "puppet:///modules/yum/etc/yum.repos.d/external.repo",
