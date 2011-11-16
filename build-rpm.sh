e9adf1508953462a4e6c21422d763d9e6e8da885
Michael F. Canzoneri <mikecanzoneri@gmail.com>
2011-11-16 04:08:06 +0000

    Expand the yum repo a bit

--- build-rpm.sh
+++ build-rpm.sh
@@ -7,4 +7,5 @@ RPM_VERSION=$3
 fpm -s dir -t rpm -v $RPM_VERSION -a noarch --prefix /etc/puppet modules/ manifests/
 mv *.rpm /yumvol/yum/amazonami/2011-09/noarch
 sudo createrepo /yumvol/yum/amazonami/2011-09/noarch
+sudo createrepo /yumvol/yum/amazonami/2011-09/x86_64
 rsync -avz --delete /yumvol/yum/ $YUM_HOST:$YUM_PATH
