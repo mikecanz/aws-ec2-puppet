8a6a63e3451a99c43d028d540dc5ada25bb2f911
Michael F. Canzoneri <mikecanzoneri@gmail.com>
2011-12-31 21:59:12 +0000

    Add perl-Dancer-Plugin-REST

--- modules/packages/manifests/perl-dancer.pp
+++ modules/packages/manifests/perl-dancer.pp
@@ -3,4 +3,8 @@ class packages::perl-dancer inherits packages {
     package { "perl-Dancer":
         ensure => installed,
     }
+
+    package { "perl-Dancer-Plugin-REST":
+        ensure => installed,
+    }
 }
