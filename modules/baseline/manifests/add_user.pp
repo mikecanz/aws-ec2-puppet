f4c7e85945df64cc93fcfb2193bb0b0cbc9e7f53
David Bishop <david@gnuconsulting.com>
2012-01-21 00:15:08 -0500

    add add_user and disable_user definitions to baseline

---
+++ modules/baseline/manifests/add_user.pp
@@ -0,0 +1,33 @@
+
+define baseline::add_user ( $uid ) {
+
+    File { owner => $name, group => $name, mode => 644, }
+
+    user { $name:
+            home     => "/home/$name",
+            shell    => "/bin/bash",
+            uid      => $uid,
+            password => template("passwords/$name"),
+    }
+
+    group { $name:
+            gid     => $uid,
+    }
+
+    file { "/home/$name/":
+            ensure  => directory,
+            require => [ user[$name], group[$name] ],
+    }
+
+    file { "/home/$name/.ssh":
+            ensure  => directory,
+            mode    => 700,
+    }
+
+    # now make sure that the ssh key authorized files is around
+    file { "/home/$name/.ssh/authorized_keys":
+            ensure  => present,
+            mode    => 600,
+    }
+
+}
