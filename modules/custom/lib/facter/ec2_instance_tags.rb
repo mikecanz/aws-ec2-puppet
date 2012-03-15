56da997653f64cccd8697c2dda95767590ff1829
David Bishop <david@gnuconsulting.com>
2012-03-15 01:42:37 -0400

    update custom fact ec2_instance_tags to be more robust

--- modules/custom/lib/facter/ec2_instance_tags.rb
+++ modules/custom/lib/facter/ec2_instance_tags.rb
@@ -1,13 +1,28 @@
 # This fact returns a comma-separated list of all tags 
 # associated with the current instance
 
-Facter.add("ec2_instance_tags") do
-    setcode do
-        if File.executable?("/usr/local/bin/fetch_ec2_tags.pl")
-            result = `/usr/local/bin/fetch_ec2_tags.pl`
-        else
-            result = False
+if File.executable?("/usr/local/bin/fetch_ec2_tags.pl")
+    fetched = `/usr/local/bin/fetch_ec2_tags.pl`.chomp
+    tags = fetched.split(/;/)
+else
+    tags = ""
+end
+
+if tags
+
+    tags.each do |x|
+        tag = Hash[*x.split(/:/)]
+        tag.each do |key,value|
+           Facter.add("ec2_instance_tag_" + key ) do
+                setcode do
+                    value
+                end
+            end
         end
-        result
     end
+
+else
+
+    return 
+
 end
--- modules/puppet/files/usr/local/bin/puppet_run.sh
+++ modules/puppet/files/usr/local/bin/puppet_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tags = `facter -p ec2_instance_tags`
+
 yum clean all 2>&1 > /dev/null
 /usr/bin/yum -y upgrade aws-ec2-puppet aws-ec2-puppet-private-accessium 2>&1 > /tmp/yum.log
 /usr/bin/puppet apply --verbose --modulepath /etc/puppet/modules /etc/puppet/manifests/nodes.pp 2>&1 > /tmp/puppet.log
