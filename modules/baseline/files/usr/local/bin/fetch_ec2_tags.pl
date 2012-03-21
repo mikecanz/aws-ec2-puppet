2f0352d6ae981c0caff7d1732b94dc6bcc8c6716
David Bishop <david@gnuconsulting.com>
2012-03-21 08:02:22 -0400

    switch fetch_ec2_tags.pl over to using ec2_instance_id_cached

--- modules/baseline/files/usr/local/bin/fetch_ec2_tags.pl
+++ modules/baseline/files/usr/local/bin/fetch_ec2_tags.pl
@@ -13,7 +13,7 @@ my $config = Config::INI::Reader->read_file('/opt/aws/credential-file');
 my $ec2 = Net::Amazon::EC2->new(AWSAccessKeyId  => $config->{'_'}->{'AWSAccessKeyId'},
                                 SecretAccessKey => $config->{'_'}->{'AWSSecretKey'});
 
-chomp(my $instance_id = `facter ec2_instance_id`);
+chomp(my $instance_id = `facter ec2_instance_id_cached`);
 
 my $result = $ec2->describe_tags({'Filter.Name' => 'resource-id', 'Filter.Value' => $instance_id});
 
