d62ef29a77559fd02f48e54749a901358c7f5a53
Michael F. Canzoneri <mikecanzoneri@gmail.com>
2013-01-12 14:58:06 -0500

    Fix the fpm command in build-rpm.sh

--- build-rpm.sh
+++ build-rpm.sh
@@ -4,7 +4,7 @@ YUM_HOST=$1
 YUM_PATH=$2
 RPM_VERSION=$3
 
-fpm -s dir -t rpm -v $RPM_VERSION -a noarch --prefix /etc/puppet modules/ manifests/
+fpm -n aws-ec2-puppet -s dir -t rpm -v $RPM_VERSION -a noarch --prefix /etc/puppet modules/ manifests/
 mv *.rpm /yumvol/yum/amazonami/2011-09/noarch
 sudo createrepo /yumvol/yum/amazonami/2011-09/noarch
 sudo createrepo /yumvol/yum/amazonami/2011-09/x86_64
